63d74401196e5acb797c0d3fada564ce0be7adb3
imvelvet22 <clariezabautista@gmail.com>
2024-01-20 04:16:49 +0900

    Fifth
    Home

--- appointmentsettler/src/components/Hero/Hero.jsx
+++ appointmentsettler/src/components/Hero/Hero.jsx
@@ -1,76 +1,60 @@
 import React from 'react';
 import css from './Hero.module.css';
-import HeroImg from '../../assets/HeroImg.png'
-import {FaHeartCirclePlus} from "react-icons/fa6";
-import { IoIosArrowDropright } from "react-icons/io";
+import HeroImg from '../../assets/HeroImg.png';
+import { FaHeartCirclePlus } from 'react-icons/fa6';
+import { IoIosArrowDropright } from 'react-icons/io';
+import { IoHeartHalf } from 'react-icons/io5';
+import { HiOutlineArrowSmallRight } from 'react-icons/hi2';
 
 const Hero = () => {
   return (
     <div className={css.container}>
-
       {/* Left side */}
       <div className={css.h_sides}>
-      <span className={css.text1}>Flawless Hair</span>
+        <span className={css.text1}>Captivating Beauty</span>
 
         <div className={css.text2}>
-          <span>Love your Hair</span>
-          <span className={css.text1}>Let your hair tell your story</span>
-
+          <span>Love Your Self</span>
+          <span className={css.text1}>Let Your Salon Tell Your Story</span>
         </div>
       </div>
 
-     {/* Middle side */}
-     <div className={css.wrapper}>
-        	<div className={css.blueCircle}>
-            <img src={HeroImg} alt="" width={600} />
-            <div className={css.cart2}>
-                <FaHeartCirclePlus/>
-
-                <div className={css.signup}>
-                    <span>Book Appointment</span>
-
-                    <div>
-                    <IoIosArrowDropright />
-                    </div>
-
-
-                </div>
-            </div>
-
-            </div>
+      {/* Middle side */}
+      <div className={css.wrapper}>
+        <div className={css.blueCircle}>
+          <img src={HeroImg} alt="" width={600} />
+        </div>
       </div>
-    {/* right side */}
+
+      {/* Right side */}
       <div className={css.h_sides}>
         <div className={css.traffic}>
-            <span>You deserve this Trend</span>
-            <span>treat your self</span>
-            <span>treat your self</span>            
-            <span>treat your self</span>
-
+          <span>You deserve this Trend</span>
+          <span>treat yourself</span>
+          <span>treat yourself</span>
+          <span>treat yourself</span>
         </div>
 
         <div className={css.customers}>
-            <span>Book Now!!!</span>
-            <span>Run Before It Runs Out</span>
-            <span>Experience Five Star Salon</span>
-
+          <span>Book Now!!!</span>
+          <span>Run Before It Runs Out</span>
+          <span>Experience Five Star Salon</span>
         </div>
-        
 
+        <div className={css.cart2try}>
+          <IoHeartHalf />
 
-        
+          <div className={css.signuptry}>
+            <span>Book Appointment</span>
 
-
-
+            <div>
+              <HiOutlineArrowSmallRight />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
-}
+};
 
 export default Hero;
-
-
-
-/*import { FaHeartCirclePlus } from "react-icons/fa6";*/
-
-/*import { IoIosArrowDropright } from "react-icons/io"; */
