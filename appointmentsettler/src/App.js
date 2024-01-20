6f478139e38d5b7338ce8c169adaf9f9cd163bcc
shen102 <sheenrodrigazo@gmail.com>
2024-01-21 02:33:34 +0800

    update js

--- appointmentsettler/src/App.js
+++ appointmentsettler/src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Services from "./components/header/Services";
 import "./App.css";
 import Header from "./components/header/Header";
 import Hero from "./components/Hero/Hero";
@@ -8,9 +10,18 @@ import Slider from "./components/Slider/Slider";
 function App() {
   return (
     <div className ="App">
-      <Header/>
-      <Hero/>
-      <Slider/>
+      
+
+      <Router>
+        <Header />
+        <Slider/>
+        <Routes>
+          <Route path="/about" element={<Hero />} />
+          <Route path="/services" element={<Services />} />
+        </Routes>
+      
+      </Router>
+ 
     </div>
   );
 }
