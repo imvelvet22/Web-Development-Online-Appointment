7278e08853cb386ce3ff243e80908d01f7c12ea8
shen102 <sheenrodrigazo@gmail.com>
2024-01-22 21:38:38 +0800

    menu button

--- appointmentsettler/src/components/header/Header.jsx
+++ appointmentsettler/src/components/header/Header.jsx
@@ -1,40 +1,53 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import css from './Header.module.css';
 import Logo from '../../assets/logo.png';
 import { CgSearchLoading } from "react-icons/cg";
 import { Link } from 'react-router-dom';
-   
-
-
+import MenuIcon from './MenuIcon'; // Import the MenuIcon component
 
 const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const isMobile = windowWidth <= 700;
+
   return (
     <div className={css.container}>
       <div className={css.logo}>
-         <img src={Logo} alt="" />
-        <span>Hiraya Beauty</span>
+        <img src={Logo} alt="" />
+        <span>Good Things Here</span>
       </div>
 
       <div className={css.right}>
         <div className={css.menu}>
           <ul className={css.menu}>
             <li><Link to="/" >Home</Link></li>
-            <li><Link to="/about" >About </Link></li>
+            <li>About</li>
+            <li>New</li>
             <li>
                <Link to="/services" >Services</Link>
              </li>
-
-            <li><Link to="/appointment" >Appointment</Link></li>
-
+            <li>Promo</li>
+            <li><Link to="/slip" >ETC</Link></li>
           </ul>
         </div>
         <input type="text" className={css.search} placeholder="Search"/>
         <CgSearchLoading className={css.cart}/>
+        {isMobile ? <MenuIcon /> : null} {/* Render the MenuIcon component only if it's not a mobile screen */}
       </div>
 
     </div>
-    
-   
   );
 }
 
